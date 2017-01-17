daf01adf3e4212bf3fad0fa5ba4a632e1ee6133c
Tom Lui <tomlui1019@gmail.com>
2017-01-17 11:19:43 +0800

    Fixing the Problem of Pin and User Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, password_length: 6..8
+
+    has_many :pins
 end
